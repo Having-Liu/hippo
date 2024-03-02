64614e3c9ed37be7ec11bfa0ba20b3b9be308fbf
zizai <rch_lewis@outlook.com>
2024-03-02 17:53:49 +0800

    Initial Commit

---
+++ hippo/hippoApp.swift
@@ -0,0 +1,32 @@
+//
+//  hippoApp.swift
+//  hippo
+//
+//  Created by 自在 on 2024/3/2.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct hippoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
