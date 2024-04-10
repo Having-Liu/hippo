83e974b2b4dd08d11f9845234dc6592b31f5cbbc
刘自在 <55235403+Having-Liu@users.noreply.github.com>
2024-04-10 18:26:33 +0800

    简单尝试

---
+++ hippowatch Watch App/hippowatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  hippowatchApp.swift
+//  hippowatch Watch App
+//
+//  Created by 自在 on 2024/4/10.
+//
+
+import SwiftUI
+
+@main
+struct hippowatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
